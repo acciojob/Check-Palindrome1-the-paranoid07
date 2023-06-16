0ad4212188b45c8c3f98d3760557f6baf8ab484c
the-paranoid07 <121954633+the-paranoid07@users.noreply.github.com>
2023-06-16 21:49:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function palindrome(str){
 let start = 0, end = str.length ;
 	while (start < end ) {
-		if(str[start] !=== str[end])
+		if(str[start] !== str[end])
 			return false;
 	}
 	return true;
